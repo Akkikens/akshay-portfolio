69eb4a9ac125d909ab8b9296581cf55520ec2739
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-11-08 13:11:41 -0500

    deleted few stuff

--- components/Home/AboutMe/AboutMe.tsx
+++ components/Home/AboutMe/AboutMe.tsx
@@ -24,13 +24,8 @@ export default function AboutMe(props) {
       <div className="flex flex-col space-y-8 px-4 sm:px-0 w-full sm:w-[500px] md:w-[700px] lg:w-[900px]">
         <div className="flex flex-row items-center">
           <div className="flex flex-row items-center mr-4">
-            <ArrowIcon
-              className={
-                "flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"
-              }
-            />
+            <ArrowIcon className="flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary" />
             <span className="text-AAsecondary font-Header text-sm sm:text-xl">
-              {" "}
               01.
             </span>
             <span className="flex-none text-gray-200 opacity-85 font-bold tracking-wider text-lg sm:text-2xl pl-4">
@@ -84,36 +79,24 @@ export default function AboutMe(props) {
               </span>
             </div>
             <div className="font-Header tracking-wide flex flex-row space-x-16 justify-center lg:justify-start">
-              <div className="flex flex-row space-x-2 items-center">
-                <div className="flex flex-col space-y-4 sm:text-base text-sm">
-                  {technologies[0].map((tech, index) => (
+              {technologies.map((techGroup, groupIndex) => (
+                <div
+                  key={groupIndex}
+                  className="flex flex-col space-y-4 sm:text-base text-sm"
+                >
+                  {techGroup.map((tech, index) => (
                     <div
                       key={index}
                       className="flex flex-row items-center space-x-2"
                     >
-                      <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
+                      <ArrowIcon className="h-3 w-3 text-AAsecondary" />
                       <span className="text-gray-400 sm:text-sm text-xs">
                         {tech}
                       </span>
                     </div>
                   ))}
                 </div>
-              </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <div className="flex flex-col space-y-4 sm:text-base text-sm">
-                  {technologies[1].map((tech, index) => (
-                    <div
-                      key={index}
-                      className="flex flex-row items-center space-x-2"
-                    >
-                      <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
-                      <span className="text-gray-400 sm:text-sm text-xs">
-                        {tech}
-                      </span>
-                    </div>
-                  ))}
-                </div>
-              </div>
+              ))}
             </div>
           </div>
 
@@ -121,15 +104,16 @@ export default function AboutMe(props) {
           <div className="group relative lg:w-96 lg:h-96 md:w-72 md:h-72 md:block hidden">
             <div
               className="group-hover:translate-x-3 group-hover:translate-y-3
-               duration-300 absolute w-5/6 h-5/6 border-2 border-AAsecondary translate-x-5 
+               duration-200 absolute w-5/6 h-5/6 border-2 border-AAsecondary translate-x-5 
                translate-y-5 rounded"
             ></div>
             <div className="absolute w-5/6 h-5/6 rounded overflow-hidden">
-              <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-10 duration-300 rounded overflow-hidden"></div>
+              <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-10 duration-200 rounded overflow-hidden"></div>
               <Img
                 src={"/Portfolio-portrait-3.jpg"}
                 className={"object-contain rounded-lg"}
                 alt="My Image Not Found"
+                loading="lazy" // Lazy loading for non-critical image
               />
             </div>
           </div>
@@ -142,6 +126,7 @@ export default function AboutMe(props) {
                 src={"/Portfolio-portrait-3.jpg"}
                 className={"object-contain rounded-lg"}
                 alt="My Image Not Found"
+                loading="lazy" // Lazy loading for non-critical image
               />
             </div>
             <div className="absolute w-48 h-full bg-AAsecondary opacity-10 md:opacity-60 rounded overflow-hidden"></div>
