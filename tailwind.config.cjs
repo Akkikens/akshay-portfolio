39b76e40c65a1996cf13c74c92b7b79c63aa59cf
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2025-08-22 22:37:00 -0400

    Update Tailwind and PostCSS config, update dependencies
    Replaced postcss.config.js with postcss.config.cjs, updated references in package.json, and updated Tailwind config. Also updated TypeScript and Next.js config files and refreshed lock files to reflect new and updated dependencies.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        AAprimary: "#0b192f",
+        AAsecondary: "#64ffda",
+        MobileNavBarColor: "#112340",
+        ResumeButtonHover: "#153040",
+        StartupBackground: "#020c1b",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
 };
